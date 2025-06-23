75771d5686c0dd90f3df9e468f9395416f4e4275
maykecorrea <maykedefigueiredo@gmail.com>
2025-06-23 16:50:52 -0300

    feat22:

--- backend/server.js
+++ backend/server.js
@@ -22,15 +22,10 @@ const PORT = process.env.PORT || 4001;
 
 // ⭐ MIDDLEWARE BÁSICO
 app.use(cors({
-  origin: ['http://localhost:4001', ], 
+  origin: ['http://localhost:4001','https://zenthur.com' ], 
   credentials: true
 }));
-  origin: [
-    'http://localhost:4001',
-    
-    'https://zenthur.com'
-  ]
-
+  
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
