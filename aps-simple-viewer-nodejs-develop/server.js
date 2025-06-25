ee8d90b4dd9f0e02208cc2ab8011a32e00b28311
maykecorrea <105362883+maykecorrea@users.noreply.github.com>
2025-06-25 16:03:43 -0300

    Update server.js

--- aps-simple-viewer-nodejs-develop/server.js
+++ aps-simple-viewer-nodejs-develop/server.js
@@ -3,18 +3,14 @@ const { PORT } = require('./config.js');
 
 const app = express();
 
-// Serve arquivos estáticos da pasta wwwroot NO PREFIXO /aps
-app.use('/aps', express.static('wwwroot'));
+app.use('/aps', express.static('wwwroot')); // arquivos estáticos
+app.use('/aps/api/auth', require('./routes/auth.js'));
+app.use('/aps/api/models', require('./routes/models.js'));
 
-// Rotas API
-app.use(require('./routes/auth.js'));
-app.use(require('./routes/models.js'));
-
-// (Opcional) Redireciona raiz /aps para /aps/index.html
 app.get('/aps', (req, res) => {
-  res.redirect('/aps/index.html');
+    res.redirect('/aps/index.html');
 });
 
 app.listen(PORT, function () {
-  console.log(`Server listening on port ${PORT}...`);
+    console.log(`Server listening on port ${PORT}...`);
 });
