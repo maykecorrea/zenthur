4da913cb08d583b5531d43b424576d36f76d0ff7
maykecorrea <105362883+maykecorrea@users.noreply.github.com>
2025-06-26 13:42:27 -0300

    Update config.js

--- aps-simple-viewer-nodejs-develop/config.js
+++ aps-simple-viewer-nodejs-develop/config.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+// Adicione logo abaixo:
+console.log('APS_CLIENT_ID:', process.env.APS_CLIENT_ID);
+console.log('APS_CLIENT_SECRET:', process.env.APS_CLIENT_SECRET);
+
 let { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_BUCKET, PORT } = process.env;
 if (!APS_CLIENT_ID || !APS_CLIENT_SECRET) {
     console.warn('Missing some of the environment variables.');
